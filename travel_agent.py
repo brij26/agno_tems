34553522916ada1567a54aecbab82943be361181
brij26 <brijrpatel007@gmail.com>
2025-12-04 09:23:38 +0530

    create travel agent with multi agent system

---
+++ travel_agent.py
@@ -0,0 +1,28 @@
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.team import Team
+from dotenv import load_dotenv
+from news_agent import news_agent
+from web_search_agent import web_search_agent
+
+
+load_dotenv()
+
+llm = OpenAIChat(id="gpt-4o-mini")
+
+
+# form the team
+travel_agent = Team(
+    members=[news_agent, web_search_agent],
+    model=llm,
+    id="Travel_agent",
+    name="Travel_agent",
+    role="You are a team leader and you delegate task to members",
+    instructions=["You are an expert travel agent",
+                  "Your team can get latest news and places of interest for a particular destination",
+                  "Your task is to present the output using proper headlines"],
+    stream=True,
+    markdown=True
+)
+
+travel_agent.cli_app(stream=True, markdown=True)
